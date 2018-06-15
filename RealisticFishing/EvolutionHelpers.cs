9f1c9fb85d8ababf1e7926eb423af6e47d32353b
kevinforrestconnors <kevinforrestconnors@gmail.com>
2018-06-15 11:12:25 -0500

    started working on evolution on a new branch

---
+++ RealisticFishing/EvolutionHelpers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace RealisticFishing
+{
+    public static class EvolutionHelpers
+    {
+        
+        public static float MutationRate = 0.05f;
+
+
+        public static double GetMutatedFishLength(int fishLength, int minLength, int maxLength) {
+
+            Random rand = new Random(); //reuse this if you are generating many
+            double u1 = 1.0 - rand.NextDouble(); //uniform(0,1] random doubles
+            double u2 = 1.0 - rand.NextDouble();
+            double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+            double randNormal = fishLength + EvolutionHelpers.MutationRate * randStdNormal; // mean + stdDev * randStdNormal   random normal(mean,stdDev^2)
+
+            if (randNormal > maxLength)
+            {
+                return maxLength;
+            }
+            else if (randNormal < minLength)
+            {
+                return minLength;
+            }
+            else
+            {
+                return Math.Floor(randNormal);
+            }
+
+        }
+
+        public static FishPopulation ComputeTomorrowPopulation(FishPopulation population) {
+            
+        }
+    }
+}
---
+++ RealisticFishing/FishPopulation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RealisticFishing
+{
+
+    public class FishPopulation
+    {
+
+        public Dictionary<String, List<Fish>> fishPopulation;
+
+        public FishPopulation()
+        {
+            this.fishPopulation = new Dictionary<String, List<Fish>>();
+        }
+
+        public override String ToString() {
+            String ret = "";
+
+            for (int i = 0; i < Fish.AllFish.Count; i++)
+            {
+                List<Fish> fishOfType;
+                this.fishPopulation.TryGetValue(Fish.AllFish[i], out fishOfType);
+
+                if (fishOfType.Count > 0)
+                {
+                    ret += fishOfType[0].name + " | Number of Fish: " + fishOfType.Count + "\n";
+                }
+            }
+
+            return ret;
+        }
+    }
+}
