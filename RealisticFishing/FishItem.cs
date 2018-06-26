fea76d5e89edd429f3cdc3afde76eaf65980564c
kevinforrestconnors <kevinforrestconnors@gmail.com>
2018-06-26 14:19:28 -0500

    adds getOne()

--- RealisticFishing/FishItem.cs
+++ RealisticFishing/FishItem.cs
@@ -10,25 +10,24 @@ namespace RealisticFishing
 {
     public class FishItem : StardewValley.Object
     {
+        public int Id;
         public int MinLength;
         public int MaxLength;
         public double Length;
         public int DisplayLength;
 
-        public int stackSize;
-        public String description;
+        public String Description;
 
-        public FishItem(int id, String name, int minLength, int maxLength, double length, int quality, int stackSize) 
+        public FishItem(int id, int minLength, int maxLength, double length, int quality) 
             : base(id, 1, false, -1, quality)
         {
-            this.Name = name;
+            this.Id = id;
             this.MinLength = minLength;
             this.MaxLength = maxLength;
             this.Length = length;
             this.DisplayLength = (int)Math.Round(this.Length);
             this.Quality = quality;
-            this.description = base.getDescription();
-            this.stackSize = stackSize;
+            this.Description = base.getDescription();
             this.Category = -4;
 
             // Allows the item to be sold
@@ -38,43 +37,18 @@ namespace RealisticFishing
             this.Price = (int)(base.Price * (this.DisplayLength / 5));
         }
 
-        public override string DisplayName { get => this.Name; set => this.Name = value; }
-        public override int Stack { get => this.stackSize; set => this.stackSize = value; }
-
-
-        public override int addToStack(int amount)
-        {
-            return 1;
-        }
-
         public override void drawInMenu(SpriteBatch spriteBatch, Vector2 location, float scaleSize, float transparency, float layerDepth, bool drawStackNumber, Color color, bool drawShadow)
         {
             base.drawInMenu(spriteBatch, location, scaleSize, transparency, layerDepth, drawStackNumber, color, drawShadow);
         }
 
+        public override Item getOne() {
+            return (Item)new FishItem(this.Id, this.MinLength, this.MaxLength, this.Length, this.Quality);
+        }
+
         public override string getDescription()
         {
-            return this.description + " " + this.DisplayLength.ToString() + " in. long.";
-        }
-
-        public override Item getOne()
-        {
-            return (Item)this;
-        }
-
-        public override int getStack()
-        {
-            return this.Stack;
-        }
-
-        public override bool isPlaceable()
-        {
-            return false;
-        }
-
-        public override int maximumStackSize()
-        {
-            return 999;
+            return this.Description + " " + this.DisplayLength.ToString() + " in. long.";
         }
 
         public override int salePrice()
@@ -82,20 +56,9 @@ namespace RealisticFishing
             return base.salePrice();
         }
 
-        public override bool canBeTrashed()
-        {
-            return base.canBeTrashed();
-        }
-
-        public override string getCategoryName()
-        {
-            return base.getCategoryName();
-        }
-
         public override bool canStackWith(Item other)
         {
-            return true;
-            //return base.canStackWith(other) && this.Name == other.Name;
+            return base.canStackWith(other);
         }
     }
 }
